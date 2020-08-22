3ba6c7c0d44d57652c22f23ff548a341af8ee1ce
André Hugo Ramalho Lopes <48354725+hugo451@users.noreply.github.com>
2020-08-22 14:41:47 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -13,7 +13,6 @@ class DropColumns(BaseEstimator, TransformerMixin):
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
-        data = pd.DataFrame(data)
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
     
@@ -29,7 +28,22 @@ class DropUselessRows(BaseEstimator, TransformerMixin):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         
-        data = pd.DataFrame(data)
+        si = SimpleImputer(
+        missing_values=np.nan,  # os valores faltantes são do tipo ``np.nan`` (padrão Pandas)
+        strategy='constant',  # a estratégia escolhida é a alteração do valor faltante por uma constante
+        fill_value=0,  # a constante que será usada para preenchimento dos valores faltantes é um int64=0.
+        verbose=0,
+        copy=True
+        )
+        
+        si.fit(X=data)
+
+        data = pd.DataFrame.from_records(
+            data=si.transform(
+                X=data
+            ),  # o resultado SimpleImputer.transform(<<pandas dataframe>>) é lista de listas
+            columns=data.columns  # as colunas originais devem ser conservadas nessa transformação
+        )
         
         for i in range(0, len(data)):
             try:
@@ -43,24 +57,7 @@ class DropUselessRows(BaseEstimator, TransformerMixin):
             except:
                 continue
         
-        si = SimpleImputer(
-        missing_values=np.nan,  # os valores faltantes são do tipo ``np.nan`` (padrão Pandas)
-        strategy='constant',  # a estratégia escolhida é a alteração do valor faltante por uma constante
-        fill_value=0,  # a constante que será usada para preenchimento dos valores faltantes é um int64=0.
-        verbose=0,
-        copy=True
-        )
         
-        si.fit(X=data)
-
-# Reconstrução de um novo DataFrame Pandas com o conjunto imputado (df_data_3)
-        data_2 = pd.DataFrame.from_records(
-            data=si.transform(
-                X=data
-            ),  # o resultado SimpleImputer.transform(<<pandas dataframe>>) é lista de listas
-            columns=data.columns  # as colunas originais devem ser conservadas nessa transformação
-        )
-        
-        
+   
         # Retornamos um novo dataframe sem as linhas indesejadas
-        return data_2
+        return data
