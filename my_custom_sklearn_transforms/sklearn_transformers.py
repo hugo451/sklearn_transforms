d337e241efd793a7bd5674de6566639254d8d486
André Hugo Ramalho Lopes <48354725+hugo451@users.noreply.github.com>
2020-08-22 12:16:59 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,29 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
+    
+class DropUselessRows(BaseEstimator, TransformerMixin):
+    def __init__(self, columns, value):
+        self.columns = columns
+        self.value = value
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        
+        for i in range(0, len(data.index)):
+            try:
+                flag = 0
+                for column in self.columns:
+                    if data.iloc[i][column] == self.value:
+                        flag += 1
+                    
+                if flag == len(self.columns):
+                    data = data.drop(i)
+            except:
+                continue
+        return data
